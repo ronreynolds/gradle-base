bd74d526c5aa7194815e0f2b82c94cd0934163ea
Ron Reynolds <ron@ronreynolds.com>
2025-02-23 08:28:12 -0800

    updated library versions and included dates those versions were released

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,11 +9,11 @@ version = "0.0.1-SNAPSHOT"
 val mainClass = "com.ronreynolds.temp.Main"
 
 // library versions
-val assertJVersion           = "3.24.2"
-val guavaVersion             = "33.4.0-jre"
-val jUnitJupiterVersion      = "5.10.2"
-val lombokVersion            = "1.18.32"
-val slf4jVersion             = "1.7.25"
+val assertJVersion           = "3.27.3"		// 2025-01-18
+val guavaVersion             = "33.4.0-jre"	// 2024-12-17
+val jUnitJupiterVersion      = "5.11.4"		// 2024-12-16
+val lombokVersion            = "1.18.36"	// 2024-11-15
+val slf4jVersion             = "2.0.16"		// 2024-08-08
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_11
