b57c72da206de3f21e5aa5e890e689a83039996a
Ron Reynolds <ron@ronreynolds.com>
2025-03-30 07:34:41 -0700

    added CHANGELOG.md and cleanup of build.gradle.kts

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+# X changelog
+* based on https://keepachangelog.com/en/1.0.0/ and https://semver.org/
+* sections: **Breaking** **Added** **Changed** **Deprecated** **Fixed** **Removed** **Security** **ToDo** (in that order)
+
+## 0.0.1 - unreleased
+### Added
+* .
+### Changed
+* .
+### Deprecated
+* .
+### Fixed
+* .
+### Removed
+* .
+### ToDo
+* .
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,75 +1,76 @@
 plugins {
-	id("java")
-	id("maven-publish")
+    id("java")
+    id("maven-publish")
 }
 
-group 	= "com.ronreynolds"
+group   = "com.ronreynolds"
 version = "0.0.1-SNAPSHOT"
 
 val mainClass = "com.ronreynolds.temp.Main"
 
 // library versions
-val assertJVersion           = "3.27.3"		// 2025-01-18
-val guavaVersion             = "33.4.0-jre"	// 2024-12-17
-val jUnitJupiterVersion      = "5.11.4"		// 2024-12-16
-val lombokVersion            = "1.18.36"	// 2024-11-15
-val slf4jVersion             = "2.0.16"		// 2024-08-08
+val assertJVersion      = "3.27.3"         // 2025-01-18
+val jUnitJupiterVersion = "5.12.0"         // 2025-02-21
+val logbackVersion      = "1.5.17"         // 2025-02-25
+val lombokVersion       = "1.18.36"        // 2024-11-15
+val slf4jVersion        = "2.0.17"         // 2025-02-25
+
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_11
-	targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 repositories {
-	mavenLocal()	// so we can use some of our local libs
-	mavenCentral()
+    mavenCentral()
+    mavenLocal()
 }
 
 dependencies {
-	implementation			("org.slf4j:slf4j-api:$slf4jVersion")
-	runtimeOnly				("org.slf4j:slf4j-simple:$slf4jVersion")
+    implementation("org.slf4j:slf4j-api:$slf4jVersion")
+    runtimeOnly   ("ch.qos.logback:logback-classic:$logbackVersion")
+    
+    // test dependencies
+    testImplementation("org.assertj:assertj-core:$assertJVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter:$jUnitJupiterVersion")
+    testRuntimeOnly   ("org.junit.platform:junit-platform-launcher")
 
-	// Lombok dependencies
-	compileOnly				("org.projectlombok:lombok:$lombokVersion")
-	annotationProcessor		("org.projectlombok:lombok:$lombokVersion")
-	testCompileOnly			("org.projectlombok:lombok:$lombokVersion")
-	testAnnotationProcessor	("org.projectlombok:lombok:$lombokVersion")
-
-	// test dependencies
-	testImplementation		("org.assertj:assertj-core:$assertJVersion")
-	testImplementation		("org.junit.jupiter:junit-jupiter:$jUnitJupiterVersion")
-	testRuntimeOnly			("org.junit.platform:junit-platform-launcher")
+    // Lombok dependencies
+    compileOnly            ("org.projectlombok:lombok:$lombokVersion")
+    annotationProcessor    ("org.projectlombok:lombok:$lombokVersion")
+    testCompileOnly        ("org.projectlombok:lombok:$lombokVersion")
+    testAnnotationProcessor("org.projectlombok:lombok:$lombokVersion")
 }
 
 tasks.compileJava {
-	options.encoding = "UTF-8"
-}
-
-publishing {
-	publications {
-		create<MavenPublication>("maven") {
-			from(components["java"])
-		}
-	}
-}
-
-tasks.create("uberjar", Jar::class) {
-	group = "build"
-	description = "Creates a jar containing classes and all runtime dependencies"
-	manifest.attributes["Main-Class"] = mainClass
-	duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-	val dependencies = configurations
-		.runtimeClasspath
-		.get()
-		.map(::zipTree)
-	from(dependencies)
-	with(tasks.jar.get())
+    options.encoding = "UTF-8"
 }
 
 tasks.compileTestJava {
-	options.encoding = "UTF-8"
+    options.encoding = "UTF-8"
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
+}
+
+tasks.create("uberjar", Jar::class) {
+    group = "build"
+    description = "Creates a jar containing classes and all runtime dependencies"
+    manifest.attributes["Main-Class"] = mainClass
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    val dependencies = configurations
+        .runtimeClasspath
+        .get()
+        .map(::zipTree)
+    from(dependencies)
+    with(tasks.jar.get())
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
 }
